70f8080402b3db00a3855b74c2e3596c1d569aaf
ivan <ivan.pro.orange.@gmail.com>
2020-03-30 18:47:53 +0400

    new commit.

--- app/src/main/java/com/example/mareu/service/DummyReunionGenerator.java
+++ app/src/main/java/com/example/mareu/service/DummyReunionGenerator.java
@@ -11,15 +11,13 @@ import java.util.List;
 public abstract class DummyReunionGenerator {
 
     public static List<Reunion> generateReunionsForDemo(){
-        List<Reunion> dummy_reunions =  new ArrayList<Reunion>();
+        List<Reunion> dummy_reunions =  new ArrayList<>();
         String subject = "Mario Peach Luigi et Bower et BobBomb, Mario Peach Luigi et Bower et BobBomb, ";
-         dummy_reunions.add(new Reunion(4, 1, subject, "leoMessi@gmail.com, cristianoRonaldo@gamil.com, griezmann@gmail.com, mbappe@gmail.com, paulPogba@gmail.com", new GregorianCalendar( 2020,  2, 27,  18, 00) {}));
-        dummy_reunions.add(new Reunion(3, 3, "Peach", "pierre et paul", new GregorianCalendar( 2020,  0, 28,  18, 00) {}));
-        dummy_reunions.add(new Reunion(2, 4, "Luigi", "pierre et paul", new GregorianCalendar( 2020,  0, 29,  18, 00) {}));
-        dummy_reunions.add(new Reunion(1, 2, "Daisy", "pierre et paul", new GregorianCalendar( 2020,  0, 25,  18, 00) {}));
+         dummy_reunions.add(new Reunion(4, 1, subject, "leoMessi@gmail.com, cristianoRonaldo@gamil.com, griezmann@gmail.com, mbappe@gmail.com, paulPogba@gmail.com", new GregorianCalendar( 2020,  2, 27,  18, 10) {}));
+        dummy_reunions.add(new Reunion(3, 3, "Peach", "anthony@gmail.com, leo@gmail.com", new GregorianCalendar( 2020,  8, 28,  18, 5) {}));
+        dummy_reunions.add(new Reunion(2, 4, "Luigi", "paul-pogba@gmail.com, hugo-llori@gmail.com, gian-luigi-buffon@gmail.com", new GregorianCalendar( 2020,  8, 29,  18, 10) {}));
+        dummy_reunions.add(new Reunion(1, 2, "Daisy", "milanAc@gmail.com, liverpool@gmail.com, arsenal@gmail.com, barcelone@gmail.com, manchester@gmail.com", new GregorianCalendar( 2020,  0, 25,  18, 10) {}));
+        dummy_reunions.add(new Reunion(5, 2, "Mario", "Jean-claude-vanDamme@gmail.com, lucas-hernandez@gmail.com", new GregorianCalendar( 2020,  0, 25,  18, 25) {}));
         return dummy_reunions;
     }
-
-
-
 }
